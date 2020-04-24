182cb44df8b11304ef076746a433baf87d47fa50
Dennis <dennis.donguk.lee@gmail.com>
2020-04-24 10:10:18 +0900

    Points sort

--- closest_pair_dc-prj/closest_pair_dc-files/closestpair.cpp
+++ closest_pair_dc-prj/closest_pair_dc-files/closestpair.cpp
@@ -16,7 +16,8 @@ std::istream& operator>> (std::istream& os, Point & p) {
 }
 
 ////////////////////////////////////////////////////////////////////////////////
-float closestPair_aux ( .................... );
+bool operator<(Point const& a, Point const& b);
+float closestPair_aux (std::vector<Point> const& pts, int size);
 
 ////////////////////////////////////////////////////////////////////////////////
 float closestPair ( std::vector< Point > const& points ) {
@@ -25,18 +26,32 @@ float closestPair ( std::vector< Point > const& points ) {
 	if (size==0) throw "zero size subset";
 	if (size==1) return std::numeric_limits<float>::max();
 
-	return closestPair_aux( ....... );
+	std::vector< Point > sortedPts = points;
+	std::sort(sortedPts.begin(), sortedPts.end());
+
+	return closestPair_aux(sortedPts, size);
 }
 
 ////////////////////////////////////////////////////////////////////////////////
-float closestPair_aux ( ..................... ) {
-	int size = indices.size();
+bool operator<(Point const& a, Point const& b)
+{
+  //if (a.x == b.x)
+  //{
+		//return a.y < b.y;
+  //}
+
+	return a.x < b.x;
+}
+
+////////////////////////////////////////////////////////////////////////////////
+float closestPair_aux ( std::vector< Point > const& pts , int size)
+{
+	//int size = indices.size();
 
 	if (size==0) throw "zero size subset";
 	if (size==1) return std::numeric_limits<float>::max();
 
-	.................
 
-	return min_dist;
+	//return min_dist;
 }
 
--- closest_pair_dc-prj/closest_pair_dc-files/driver.cpp
+++ closest_pair_dc-prj/closest_pair_dc-files/driver.cpp
@@ -112,12 +112,12 @@ void test14() {
 void (*pTests[])(void) = { test0,test1,test2,test3,test4,test5,test6,test7,test8,test9,test10,test11,test12,test13,test14 };
 
 int main( int argc, char ** argv) {
-    if (argc!=2) return 1;
-    else {
-        int test = 0;
-        std::sscanf(argv[1],"%i",&test);
-        pTests[ test ]();
-    }
+    //if (argc!=2) return 1;
+    //else {
+    //    int test = 0;
+    //    std::sscanf(argv[1],"%i",&test);
+    //    pTests[ test ]();
+    //}
 
 
     //DEBUG
